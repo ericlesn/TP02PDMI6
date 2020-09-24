855245fc4ba1c7d37811f38a2a877cc2e7ffeb6d
ericlesn <ericlesnunes1@outlook.com>
2020-09-24 18:38:27 -0300

    finished

---
+++ TP02PDMI6/TP02PDMI6/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TP02PDMI6
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainPage = new NavigationPage(new View.Principal());
+            InitializeComponent();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TP02PDMI6/TP02PDMI6/View/Home.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP02PDMI6.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TP02PDMI6.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Home : ContentPage
+    {
+        public Home()
+        {
+            InitializeComponent();
+
+            var contato = new Contato
+            {
+                Nome = "Guilherme Duarte",
+                Idade = 22,
+                Profissao = "Developer",
+                Pais = "Brasil"
+            };
+
+            Nome.Text = contato.Nome;
+            Idade.Text = Convert.ToString(contato.Idade);
+            Profissao.Text = contato.Profissao;
+            Pais.Text = contato.Pais;
+
+
+        }
+    }
+}
